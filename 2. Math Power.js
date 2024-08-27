4c899bd1a26e3e5316e220809e166aa22f55afa7
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-27 22:03:27 +0300

    Add files via upload

---
+++ 2. Math Power.js
@@ -0,0 +1,7 @@
+function mathPower(num, power) {
+    let result = num ** power;
+    //let result1 = Math.pow(num, power);
+    console.log(result);
+    //console.log(result1);
+}
+mathPower(3, 4);
